4cfeee40e3c98b9bc1af13a732fb002f9bea43c6
Anugrah( a.k.a Max) <prototype.anugrah@gmail.com>
2018-07-30 18:01:26 +0530

    Update fourier_series.py

--- fourier_series.py
+++ fourier_series.py
@@ -1,23 +1,21 @@
-"for a given function of your choice, enter function in line 17 and 19"
-
 from pylab import *
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
+def fun(x):
+  f = x**2 - 2*x
+  return f
+
 def four(f,n):
-  
   a = []
   b = []
-   
   for i in range (0,1+n):
-
-    
-    result,error = quad(lambda x: (x**2-2*x)*cos((2*pi*i*x)/(2*pi)) , -pi , pi)
-    a.append(result/pi)
-    result,error = quad(lambda x: (x**2-2*x)*sin((2*pi*i*x)/(2*pi)) , -pi , pi)
-    b.append(result/pi)
+    res,err = quad(lambda x: fun(x)*cos((2*pi*i*x)/(2*pi)) , -pi , pi)
+    a.append(res/pi)
+    res,err = quad(lambda x: fun(x)*sin((2*pi*i*x)/(2*pi)) , -pi , pi)
+    b.append(res/pi)
     
   def fn(x):
     sum = a[0]/2
